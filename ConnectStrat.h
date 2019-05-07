0f50ab3f7aaed20db7ced2550c40fb3ae0691b25
Annie Wong <awong063@ucr.edu>
2019-05-06 20:16:03 -0700

    Connector Strategy Pattern created

--- ConnectStrat.h
+++ ConnectStrat.h
@@ -1,8 +1,11 @@
 #ifndef _CONNECTSTRAT_H_
 #define _CONNECTSTRAT_H_
 
+class ConnectStrat {
+	public:
+		ConnectStrat(){	}
+		virtual bool execute() = 0;	
+};
 
-
-
-
-#endif 
+#endif
+ 
---
+++ Connector.h
@@ -0,0 +1,17 @@
+#ifndef _CONNECTOR_H_ 
+#define _CONNECTOR_H_
+
+#include "Juat.h"
+
+class Connector: public Juat{
+	private:
+		ConnectStrat* s; 
+
+	public:
+		string get_Line(string input);
+		void bool run(string connector);
+
+}
+	
+
+#endif
