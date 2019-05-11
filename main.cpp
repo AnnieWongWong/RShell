9b06c24da988c224d17c7e56654d93d9e59835ed
Annie Wong <awong063@ucr.edu>
2019-05-11 13:12:46 -0700

    integration test

---
+++ main.cpp
@@ -0,0 +1,23 @@
+#include <iostream>
+#include <sstream>
+#include <vector>
+#include <unistd.h> 
+#include <stdio.h>
+#include <string.h>
+#include "header/Juat.h"
+#include "header/Executable.h"
+#include "header/SemiColon.h"
+#include "header/OrStrat.h"
+#include "header/AndStrat.h"
+#include "header/Command.h"
+
+using namespace std;
+
+
+int main() {
+
+Command start;
+start.run();
+
+
+}
