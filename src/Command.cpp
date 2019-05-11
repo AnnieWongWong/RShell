920658ce1bf0414bccb73049f3a3e59419ad8bc6
Annie Wong <awong063@ucr.edu>
2019-05-11 05:30:28 -0700

    ?

--- src/Command.cpp
+++ src/Command.cpp
@@ -1,22 +1,22 @@
 #include <iostream>
-#include "../header/Command.h"
+#include "Command.h"
 
 
 
 using namespace std;
 
+
 Command::Command(){
-  eqex = false;
-  root = NULL;
-  power = NULL;
+	
 
 }
 
-bool Command::run(){
+void Command::run(){
 	string userCommand;
 
 	this->start_Command_prompt();
 	
+	userCommand =	this->get_line();
 	//While loop to parse/tokenize userCommand
 	//While loop to go through tree using the run of primitives 
 
