e615100b0b6f3c23c527ddf2800c360d5356ab82
Annie Wong <awong063@ucr.edu>
2019-05-11 23:29:54 -0700

    fix merge conflicts

--- header/Juat.h
+++ header/Juat.h
@@ -7,12 +7,8 @@ using namespace std;
 class Juat{
 	public:
 		Juat(){}
-<<<<<<< HEAD
-   virtual bool run(bool runned) = 0;
-=======
 		virtual bool run(bool x) = 0;
 		virtual void getstring() = 0;
->>>>>>> 5fde9cb240cfe9ca685f658412f50aa760bd5590
 };
 
 
